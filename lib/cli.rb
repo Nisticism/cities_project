dcdf49bebdf8d9da9644c59f1d025bd490bc1a4d
Hans Foster <fosterhans@gmail.com>
2019-11-13 23:06:37 +0000

    loop selects, numbers, and displays all city names

--- lib/cli.rb
+++ lib/cli.rb
@@ -12,9 +12,23 @@ def start
   
   html = open("https://en.wikipedia.org/wiki/List_of_European_cities_by_population_within_city_limits")
   doc = Nokogiri::HTML(html)
-  doc.css("tbody").each do |city|
-    city_name = city.css("td").text
+  cities = []
+  doc.css("tbody")[0].css("tr").each do |city|
+    city_name = ""
+    city.css("td").each_with_index do |city_element, index|
+      if index == 1
+        city_name = city_element.text
+        city_name_array = city_name.split("[")
+        city_name = city_name_array[0]
+      end
+    end
+    if city_name != ""
+      cities << city_name
+    end
   end
   
-  binding.pry
+  cities.each.with_index(1) do |city, index|
+    puts "#{index}. #{city}"
+  end
+  
 end
