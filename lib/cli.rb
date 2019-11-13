e79943105bcfca9300b7caaf7ea348e8bc2ade2f
Hans Foster <fosterhans@gmail.com>
2019-11-13 08:24:34 +0000

    city name loop

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
+# document.querySelectorAll("tbody")[0].querySelectorAll("td")[1].innerText
+
 def start 
   puts "Welcome to City Scraper"
   puts "These are the most populous cities in Europe:"
@@ -10,5 +12,9 @@ def start
   
   html = open("https://en.wikipedia.org/wiki/List_of_European_cities_by_population_within_city_limits")
   doc = Nokogiri::HTML(html)
+  doc.css("tbody").each do |city|
+    city_name = city.css("td").text
+  end
+  
   binding.pry
 end
