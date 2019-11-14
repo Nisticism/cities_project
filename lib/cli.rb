adfa0cf02ac35635636c7e0aa45558fa0a035521
Hans Foster <fosterhans@gmail.com>
2019-11-14 03:43:48 +0000

    Inputs are strings

--- lib/cli.rb
+++ lib/cli.rb
@@ -16,25 +16,22 @@ class CLI
     menu
     
     input = gets
-    if input == /[0-9]/
-      input = input.to_i
-    end
-    if input == 1
+    if input == '1'
       print_cities
       puts "press any key to continue..."
       STDIN.getch 
       start
-    elsif input == 2 
+    elsif input == '2' 
       print_urls
-    elsif input == 3
+    elsif input == '3'
       print_countries
-    elsif input == 4
+    elsif input == '4'
       print_populations
-    elsif input == 5
+    elsif input == '5'
       print_countries_with_cities
-    elsif input == 6
+    elsif input == '6'
       print_city_descriptions
-    elsif input == 7
+    elsif input == '7'
       exit!
     else
       puts "Try again. Please enter any number from 1 - 7"
